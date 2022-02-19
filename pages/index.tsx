8aa0a54045a07ac9c5549a9680a69f94978479c4
unknown <rnrudxo2872@naver.com>
2022-02-19 17:02:34 +0900

    tailwindcss 셋팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>hi</div>;
+  return (
+    <div className="bg-red-500">
+      <div className="text-black">Hi</div>
+    </div>
+  );
 };
 
 export default Home;
