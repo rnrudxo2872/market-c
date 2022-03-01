b9f535bb026cdd42032a6386602428ad8280f201
unknown <rnrudxo2872@naver.com>
2022-03-02 00:06:30 +0900

    tailwind css test - implement one component2 (Ecommerce elements)

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,35 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white py-9 px-36 rounded-lg shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl flex flex-col h-60 w-96">
+        <div className="pt-10 pb-14 bg-sky-400 px-7 flex justify-between">
+          <span>Profile</span>
+          <span></span>
+        </div>
+        <div className="bg-white -mt-7 rounded-t-3xl w-full h-full px-7 flex justify-between">
+          <div className="flex flex-col mt-2">
+            <span>Order</span>
+            <span className="font-bold">340</span>
+          </div>
+          <div className="-mt-7 flex flex-col space-y-3">
+            <div className="rounded-full bg-slate-400 w-14 h-14 mx-auto"></div>
+            <div className="flex flex-col align-middle text-center">
+              <span>Tony Molly</span>
+              <span>New York, USA</span>
+            </div>
+          </div>
+          <div className="flex flex-col mt-2">
+            <span>Spent</span>
+            <span className="font-bold">
+              {Number.prototype.toLocaleString.call(2310, "en-US", {
+                style: "currency",
+                currency: "USD",
+                maximumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </div>
+      </div>
       <div className="bg-white py-9 px-36 rounded-lg shadow-xl"></div>
       <div className="bg-white py-9 px-36 rounded-lg shadow-xl"></div>
       <div className="bg-white py-9 px-36 rounded-lg shadow-xl"></div>
