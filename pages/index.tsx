3700c38eee651077cce3ff4e7dd8476cb2b0c30a
unknown <rnrudxo2872@naver.com>
2022-03-05 17:54:49 +0900

    profile 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,7 @@ const Home: NextPage = () => {
     <div className="bg-slate-300 py-28 px-60 flex flex-col space-y-5">
       <ClacResult />
       <UserProfile />
-      <div className="bg-white py-9 px-36 rounded-lg shadow-xl">
-        <RadioColor />
-      </div>
+      <div className="bg-white py-9 px-36 rounded-lg shadow-xl"></div>
       <div className="bg-white py-9 px-36 rounded-lg shadow-xl"></div>
     </div>
   );
