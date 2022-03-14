3bacfb61f9bc71aeac66dfe4814ba3a8a4a0a526
unknown <rnrudxo2872@naver.com>
2022-03-15 00:11:18 +0900

    apiEnter - create user

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,7 +1,17 @@
+import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function enterHandler(req: NextApiRequest, res: NextApiResponse) {
+  const { email, phone } = req.body;
+  const method = email ? { email } : { phone: Number(phone) };
+
+  const user = await client.user.upsert({
+    where: { ...method },
+    create: { name: "Unknown", ...method },
+    update: {},
+  });
+
   res.status(200).json({ ok: true });
 }
 
