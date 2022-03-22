46e5610f7597ac80be465a95a36eca122b079211
unknown <rnrudxo2872@naver.com>
2022-03-22 22:26:33 +0900

    product - model 추가

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+declare global {
+  var client: PrismaClient;
+}
+
+//hot-loading 으로 인한 여러 인스턴스 생성 방지
+const client = global.client || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") {
+  global.client = client;
+}
+
+export default client;
