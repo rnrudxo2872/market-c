29ef9badb14a21fec08a5e3c7af6704463206c81
unknown <rnrudxo2872@naver.com>
2022-04-08 23:33:16 +0900

    communityWonder - '궁금해요' 중복생성 방지

--- pages/api/community/[id]/wonder.ts
+++ pages/api/community/[id]/wonder.ts
@@ -1,10 +1,7 @@
 import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 import { withSession } from "@libs/server/withSession";
-import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
-
-const prisma = new PrismaClient();
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const {
@@ -24,42 +21,29 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       communityPostId: Number(id.toString()),
       userId: user!.id,
     },
-    select: {
-      id: true,
-    },
-  });
-  console.log("isExist? --> ", isExist);
-
-  const deleted = client.wonder.deleteMany({
-    where: {
-      communityPostId: Number(id.toString()),
-      userId: user?.id,
-    },
   });
 
   if (isExist) {
-    await prisma.$transaction([deleted]);
-    return res.status(201).json({
-      ok: true,
+    await client.wonder.delete({
+      where: {
+        id: isExist.id,
+      },
+    });
+  } else {
+    const nowUser = await client.user.findUnique({
+      where: {
+        id: user!.id,
+      },
+    });
+    await client.wonder.create({
+      data: {
+        id: `${nowUser?.email ?? nowUser?.phone}/${Number(id.toString())}`,
+        userId: user!.id,
+        communityPostId: Number(id.toString()),
+      },
     });
   }
 
-  const created = client.wonder.create({
-    data: {
-      user: {
-        connect: {
-          id: user!.id,
-        },
-      },
-      communityPost: {
-        connect: {
-          id: Number(id.toString()),
-        },
-      },
-    },
-  });
-
-  await prisma.$transaction([deleted, created]);
   return res.status(201).json({
     ok: true,
   });
