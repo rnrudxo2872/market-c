e58d1addfa955b5e490f3e9515b408d54be75cc4
unknown <rnrudxo2872@naver.com>
2022-04-18 00:03:09 +0900

    profile - 바뀐 프로필 사진 미리보기

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -4,13 +4,14 @@ import Layout from "@components/layout";
 import useMutation from "@libs/client/useMutation";
 import useUser from "@libs/client/useUser";
 import { NextPage } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ErrorOption, useForm } from "react-hook-form";
 
 interface IFromData {
   name: string | null;
   email: string | null;
   phone: string | null;
+  avatar: FileList | null;
   formError: string | null;
 }
 
@@ -109,59 +110,81 @@ const Edit: NextPage = () => {
 
   watch(() => clearErrors());
 
+  const avatar = watch("avatar");
+  const [previewAvatar, setPreviewAvatar] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (avatar && avatar.length) {
+      setPreviewAvatar(URL.createObjectURL(avatar[0]));
+    }
+  }, [avatar]);
+
   return (
     <Layout hasBackBtn>
-      <div className="pt-10 space-y-5">
+      <form className="pt-10 space-y-5" onSubmit={handleSubmit(onValid)}>
         <section className="flex justify-center">
-          <div className="aspect-square w-24 bg-slate-400 rounded-full relative">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 absolute bottom-0.5 right-1 border border-gray-300 rounded-full bg-white text-gray-600 p-[0.1rem]"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z"
-                clipRule="evenodd"
+          <div className="aspect-square w-24 relative">
+            {previewAvatar ? (
+              <img
+                src={previewAvatar}
+                alt={`${user?.email}의 프로필 이미지`}
+                className="w-full h-full rounded-full"
               />
-            </svg>
+            ) : (
+              <div className="w-full h-full bg-slate-400 rounded-full" />
+            )}
+            <label htmlFor="changePicture">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 absolute bottom-0.5 right-1 border border-gray-300 rounded-full bg-white text-gray-600 p-[0.1rem]"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input
+                id="changePicture"
+                accept="image/*"
+                type="file"
+                className="hidden"
+                {...register("avatar")}
+              />
+            </label>
           </div>
         </section>
-        <section>
-          <form
-            className="space-y-4 flex flex-col"
-            onSubmit={handleSubmit(onValid)}
-          >
-            <InputWithLabel
-              id="name"
-              labelText="Name"
-              type="text"
-              register={register("name")}
-            />
-            <InputWithLabel
-              id="email"
-              labelText="Email address"
-              type="email"
-              register={register("email")}
-            />
-            <InputWithLabel
-              id="phone"
-              labelText="Phone number"
-              type="phone"
-              register={register("phone")}
-            />
-            {errors.formError ? (
-              <span className="text-red-400 mx-auto">
-                {errors.formError?.message}
-              </span>
-            ) : null}
-            <BaseBtn OnClick={() => {}}>
-              {loading ? "당근 캐는중..." : "완료"}
-            </BaseBtn>
-          </form>
+        <section className="space-y-4 flex flex-col">
+          <InputWithLabel
+            id="name"
+            labelText="Name"
+            type="text"
+            register={register("name")}
+          />
+          <InputWithLabel
+            id="email"
+            labelText="Email address"
+            type="email"
+            register={register("email")}
+          />
+          <InputWithLabel
+            id="phone"
+            labelText="Phone number"
+            type="phone"
+            register={register("phone")}
+          />
+          {errors.formError ? (
+            <span className="text-red-400 mx-auto">
+              {errors.formError?.message}
+            </span>
+          ) : null}
+          <BaseBtn OnClick={() => {}}>
+            {loading ? "당근 캐는중..." : "완료"}
+          </BaseBtn>
         </section>
-      </div>
+      </form>
     </Layout>
   );
 };
